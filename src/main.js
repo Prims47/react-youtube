30e3cad2abdd91fc3a82ed645ef2c0df3b894fcd
Ilan Benichou <ibenichou@ekino.com>
2016-04-19 00:41:19 +0200

    Initialize search bar with Youtube search api

--- src/main.js
+++ src/main.js
@@ -1,12 +1,44 @@
+import _ from 'lodash';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
 
-class Test extends React.Component {
+const API_KEY = 'xxx';
+
+/*const App = () => {
+    return (
+        <div className="row center-block">
+            <div className="col-sm-12"><SearchBar /></div>
+        </div>
+    );
+};*/
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {videos: []};
+
+        this._videoSearch('cat');
+    }
+
+    _videoSearch(keyword) {
+        YTSearch({key: API_KEY, term: keyword}, (videos) => {
+            this.setState({videos})
+            console.log(videos);
+        });
+    }
+
     render () {
+        const videoSearch = _.debounce((keyword) => {this._videoSearch(keyword)}, 300);
+
         return (
-            <div>Initial</div>
+            <div className="row center-block">
+                <div className="col-sm-12"><SearchBar searchKeyword={videoSearch}/></div>
+            </div>
         );
     }
 }
 
-ReactDOM.render(<Test />, document.getElementById('container'));
+ReactDOM.render(<App />, document.getElementById('container'));
